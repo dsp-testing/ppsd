3f58e114167f0f7437c3457e31c5aa0452ee0439
Ankit Honey <honeyankit@github.com>
2022-08-03 09:22:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,12 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
+        url='https://github.com/dsp-testing/ppsd',
+        project_urls={
+          "Documentation": "https://github.com/dsp-testing/ppsd",
+          "Source Code": "https://github.com/dsp-testing/ppsd",
+        },
         install_requires=[], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
-        
         keywords=['python', 'first package'],
 )
